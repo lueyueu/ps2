c5d122446b3abad09acdcdec513b65578d611b6b
luyue <luyue@wifi-roaming-128-4-204-3.host.udel.edu>
2016-09-22 19:07:10 -0400

    comment

---
+++ PS2/src/main/java/base/MyInteger.java
@@ -0,0 +1,87 @@
+package base;
+
+public class MyInteger {
+	
+	private int iValue;
+	
+	public MyInteger(int i){
+		iValue=i;
+	}
+	
+	public int getiValue(){
+		return iValue;
+	}
+	
+	// first set
+	public boolean isEven(){
+	
+		return (iValue%2==0?true:false);
+	}
+	
+	public boolean isOdd(){
+		
+		return (iValue%3==0?true:false);
+	}
+	
+	public boolean isPrime(){
+		if (iValue>2){
+			for (int i=2;i<=iValue/2;i++){
+				if (iValue%i==0){
+					return false;
+				}	
+			}
+		}
+		return true;
+		
+	}
+	
+	// second set
+	public static boolean isEven(int i){
+		return (i%2==0?true:false);
+	}
+	public static boolean isOdd(int i){
+		return (i%3==0?true:false);
+	}
+	
+	public static boolean isPrime(int i){
+		
+		if (i>2){
+			for (int d=2;d<=i/2;d++){
+				if (i%d==0){
+					return false;
+				}	
+			}
+		}
+		return true;
+		
+	}
+	// third set
+	public static boolean isEven(MyInteger i){
+		return i.isEven();	
+	}
+	
+	public static boolean isOdd(MyInteger i){
+		return i.isOdd();
+	}
+	public static boolean isPrime(MyInteger i){
+		if (i.iValue>2){
+			for (int d=2;d<=i.iValue/2;d++){
+				if (i.iValue%d==0){
+					return false;
+				}	
+			}
+		}
+		return true;
+		
+	}
+	
+	//Method equrals
+	public boolean equals(int i){
+		return (i==iValue?true:false);
+	}
+	public boolean equals(MyInteger i){
+		return (i.equals(iValue));
+		}
+}
+			
+
---
+++ PS2/src/test/java/base/MyInteger_Test.java
@@ -0,0 +1,97 @@
+package base;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MyInteger_Test {
+	MyInteger firstNbr=new MyInteger(8);
+	MyInteger SecondNbr=new MyInteger(9);
+	MyInteger thirdNbr=new MyInteger(8);
+	MyInteger fourthNbr=new MyInteger(7);
+	MyInteger sixNbr=new MyInteger(1);
+	
+	@Test
+	public void test() {
+
+		
+		//First set
+		assertTrue(firstNbr.isEven());
+		assertFalse(SecondNbr.isEven());
+		
+		assertFalse(firstNbr.isOdd());
+		assertTrue(SecondNbr.isOdd());
+		
+
+		assertTrue(sixNbr.isPrime());
+		assertTrue(fourthNbr.isPrime());
+		assertFalse(firstNbr.isPrime());
+		assertFalse(SecondNbr.isPrime());
+		
+	}
+	
+	@Test
+	public void test2(){
+		
+		
+		//Second set
+		assertTrue(MyInteger.isEven(8));
+		assertFalse(MyInteger.isEven(9));
+		
+		
+		assertFalse(MyInteger.isOdd(8));
+		assertTrue(MyInteger.isOdd(9));
+		
+		assertTrue(MyInteger.isPrime(1));
+		assertTrue(MyInteger.isPrime(7));
+		assertFalse(MyInteger.isPrime(8));
+		assertFalse(MyInteger.isPrime(9));
+		
+	}
+	
+	@Test
+	public void test3(){
+		
+		
+		// Third set
+		
+		assertTrue(MyInteger.isEven(firstNbr));
+		assertFalse(MyInteger.isEven(SecondNbr));
+		
+		
+		assertFalse(MyInteger.isOdd(firstNbr));
+		assertTrue(MyInteger.isOdd(SecondNbr));
+		
+		assertTrue(MyInteger.isPrime(sixNbr));
+		assertTrue(MyInteger.isPrime(fourthNbr));
+		assertFalse(MyInteger.isPrime(firstNbr));
+		assertFalse(MyInteger.isPrime(SecondNbr));
+		
+		
+	}
+	
+	@Test
+	public void test4(){
+		
+		// Method equrals
+		assertTrue(firstNbr.equals(firstNbr));
+		assertTrue(SecondNbr.equals(SecondNbr));
+		assertTrue(thirdNbr.equals(thirdNbr));
+		assertFalse(thirdNbr.equals(sixNbr));
+		
+		
+		assertEquals(firstNbr.getiValue(),8);
+		assertEquals(SecondNbr.getiValue(),9);
+		assertEquals(thirdNbr.getiValue(),8);
+	
+		
+		
+	}
+	
+	
+
+}
